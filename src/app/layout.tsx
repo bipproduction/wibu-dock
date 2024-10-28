305791f4a8f37a58aceb873e677a0f8390f5b445
bipproduction <bip.production.js@gmail.com>
2024-10-28 20:43:34 +0800

    tambahan meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,26 @@
 // All packages except `@mantine/hooks` require styles imports
 import "@mantine/core/styles.css";
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+
 export const metadata = {
-  title: "Wibu Base",
-  description: "Base Wibu Project"
+  title: "Wibu Dock",
+  description: "Wibu Dock",
+  keywords: "Dock Station Wibu App", // Add relevant keywords
+  author: "Wibu Team", // Specify the author or company name
+  viewport: "width=device-width, initial-scale=1.0, minimum-scale=1, shrink-to-fit=no", // Untuk pengalaman pengguna mobile yang lebih baik
+  charset: "UTF-8", // Set the character encoding
+  themeColor: "#000000", // Define theme color for mobile browsers
+  ogTitle: "Wibu Dock", // Open Graph title
+  ogDescription: "Comprehensive Dock for Wibu web app.", // Open Graph description
+  ogUrl: "https://wibu-dock.ravenstone.cloud", // Your website URL
+  ogImage: "https://wibu-dock.ravenstone.cloud/og-image.png", // URL of an image for social media sharing
+  ogType: "website", // Open Graph type
+  ogSiteName: "Wibu Dock",
+  twitterCard: "https://wibu-dock.ravenstone.cloud/og-image.png", // Twitter card type for large image preview
+  twitterSite: "@yourTwitterHandle", // Your Twitter handle
+  twitterCreator: "@yourTwitterHandle", // Twitter creator (if applicable),
+  robots: "index, follow", // Mengizinkan mesin pencari untuk mengindeks dan mengikuti tautan
+  canonical: "https://wibu-dock.ravenstone.cloud", 
 };
 
 export default function RootLayout({
---
+++ src/app/og-image/route.tsx
@@ -0,0 +1,30 @@
+import { ImageResponse } from "next/og";
+
+export async function GET() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: "green",
+          color: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          textAlign: "center",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <div style={{
+          fontSize: 128,
+        }}>WIBU DOCK</div>
+        <p style={{ fontSize: 24 }}>Comprehensive Dock for Wibu web app.</p>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600
+    }
+  );
+}
